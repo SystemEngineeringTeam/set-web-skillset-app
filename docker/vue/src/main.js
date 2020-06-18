5907c7a2cf9c9009c4ad157dccba892374474584
Sken <k18056kk@aitech.ac.jp>
2020-06-18 23:43:55 +0900

    【ADD】vuetify,vue-router,mdi,vuexの追加

--- docker/vue/src/main.js
+++ docker/vue/src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import vuetify from "./plugins/vuetify";
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: h => h(App)
+}).$mount("#app");
