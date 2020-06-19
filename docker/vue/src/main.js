02921eb28085d718a0a8f786c68dd5fffc33bbc8
Sken <k18056kk@aitech.ac.jp>
2020-06-20 03:50:50 +0900

    【ADD】ユーザ一人一人表示するカードの追加

--- docker/vue/src/main.js
+++ docker/vue/src/main.js
@@ -1,13 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
-import VueRouter from "vue-router";
 import vuetify from "./plugins/vuetify";
-
-Vue.use(VueRouter);
+import router from "./router";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   vuetify,
   render: h => h(App)
 }).$mount("#app");
---
+++ docker/vue/src/pages/test.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="test">
+        testaaaaaaaaaa
+    </div>
+</template>
