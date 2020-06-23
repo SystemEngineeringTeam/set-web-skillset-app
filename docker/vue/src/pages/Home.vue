656725712b1a0cfcf4ed394248694df10f9d6938
Sken <k18056kk@aitech.ac.jp>
2020-06-23 21:59:22 +0900

    【FIX】ファイル名の変更

---
+++ docker/vue/src/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-container > <!-- fluidを付けることで左右いっぱいにコンテナが広がります -->
+        <!-- fill-heightを付けるとここに書いた要素は上下中央寄せで表示されます -->
+        <Filters/> 
+        <UserCard/>
+    </v-container>
+</template>
+
+<script>
+
+import UserCard from '../components/user_card';
+import Filters from '../components/filter';
+  export default {
+    components: {
+        UserCard,
+        Filters
+    },
+    data: () => ({
+
+    }),
+  }
+</script>
