dfdcb8babfa50243485537bec3f40fa8b85f5529
Sken <k18056kk@aitech.ac.jp>
2020-07-02 23:34:34 +0900

    【FIX】フィルターのデータ処理の変更

--- docker/vue/src/components/filter.vue
+++ docker/vue/src/components/filter.vue
@@ -8,61 +8,48 @@
                         solo
                         ></v-select>
                 </v-col>
+                {{this.$store.state.filter_grade}}
+                {{this.filter_data}}
                 <v-col class="d-flex" cols="12" xs='2' sm="2" md='2' v-for="(filter_item,i) in filter_items" :key="i">
                         <v-select
+                        v-model="filter_data[i]"
                         :items="filter_item.filter"
                         :label="filter_item.title"
+                        @change="obj.title"
                         solo
                         ></v-select>
                 </v-col>
         </v-layout>
 </template>
 <script>
+import { mapMutations } from 'vuex'
+
 export default {
   data: () => ({
-          cnt:0,
-          filter_items:[]
+          filter_data:[],
+          filter_items:[],
+          obj:[]
   }),
   created(){
-          this.filter_add()
+          this.filter_add(),
+          this.log_add()
   },
-  methods: {
+  methods: { 
+  log(){
+          console.log("test");
+  },
+  log_add(){
+          this.obj.push({title:this.log()})
+          },
   filter_add(){
-        this.filter_items.push({title:"学年選択",filter:this.$store.state.grade},
-                                {title:"専攻選択",filter:this.$store.state.major},
-                                {title:"所属選択",filter:this.$store.state.group},
-                                {title:"言語選択",filter:this.$store.state.technology},
-                                {title:"技術選択",filter:this.$store.state.technical_area});
+        this.filter_items.push({title:"学年選択",filter:this.$store.state.grade,set_filter:this.update_filter_grade(this.filter_data[0])},
+                                {title:"専攻選択",filter:this.$store.state.major,set_filter:this.update_filter_major(this.filter_data[1])},
+                                {title:"所属選択",filter:this.$store.state.group,set_filter:this.update_filter_group(this.filter_data[2])},
+                                {title:"言語選択",filter:this.$store.state.technology,set_filter:this.update_filter_technology(this.filter_data[3])},
+                                {title:"技術選択",filter:this.$store.state.technical_area,set_filter:this.update_filter_technical_area(this.filter_data[4])});
         },
-//       filter(filters) {
-//         for(var i = 0; i < filters.length; i++){
-//                 if(filters[i] === '学年')
-//                 {
-//                         this.$store.state.show_filter.grade = true
-//                 }
-//                 else if(filters[i] === '専攻')
-//                 {
-//                         this.$store.state.show_filter.major = true
-//                 }
-//                 else if(filters[i] === '役員')
-//                 {
-//                         this.$store.state.show_filter.position = true
-//                 }
-//                 else if(filters[i] === '所属')
-//                 {
-//                         this.$store.state.show_filter.group = true       
-//                 }
-//         }
-// if(filters.indexOf("学年") == -1)
-// {
-//         this.$store.state.show_filter.grade = false
+  ...mapMutations(['update_filter_grade','update_filter_major','update_filter_group','update_filter_technology','update_filter_technical_area'])
 
-// }
-// if(filters.indexOf("専攻") == -1)
-// {
-//         this.$store.state.show_filter.major = false
-// }
-//       }
     },
 };
 </script>
