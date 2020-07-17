aba8e36ea694b2a07136611815e03c2046e3b6c7
Sken <k18056kk@aitech.ac.jp>
2020-07-17 22:20:36 +0900

    【ADD】cardのcomponentを追加

--- docker/vue/src/components/user_card.vue
+++ docker/vue/src/components/user_card.vue
@@ -1,69 +1,38 @@
 <template>
-  <v-layout wrap>
-    <!-- wrap属性をつけるのがポイント -->
-    <v-flex class="d-flex" xs="12" sm="6" md="4">
-      <v-row>
-        <template v-if="this.$store.state.change_display === '学年'">
-          <Grade />
-        </template>
-        <template v-else>
-          <!-- {{this.$store.state.users}} -->
-          <div v-for="user in this.$store.state.users" :key="user.id">
-            <!-- {{user}} -->
-            <v-col cols="12">
-              <v-hover v-slot:default="{ hover }" open-delay="200">
-                <v-card
-                  v-if="
-                    $store.state.filter_grade === user.grade ||
-                      (!$store.state.filter_grade && !$store.state.filter_major)
-                  "
-                  :elevation="hover ? 16 : 2"
-                  class="d-flex mx-auto card"
-                  max-width="400"
-                >
-                  <v-responsive
-                    class="text-center grey lighten-2 rounded-circle d-inline-flex align-center justify-center ma-3"
-                    height="108"
-                    width="108"
-                  >
-                    <v-img v-bind:src="user.img"></v-img>
-                  </v-responsive>
-                  <v-card-text>
-                    <p class="display-1 text--primary">{{ user.name }}</p>
-                    <p>{{ user.grade }}年 {{ user.major }} {{ user.group }}</p>
-                    <div class="text--primary">
-                      最近はサーバー構築やってるよ!
-                      <br />気軽に話しかけてね
-                    </div>
-                    <v-card-actions></v-card-actions>
-                  </v-card-text>
-                </v-card>
-              </v-hover>
-            </v-col>
-          </div>
-        </template>
-      </v-row>
-    </v-flex>
-    <!--<v-flex xs='12' sm='6' md='4'>コンテンツ</v-flex>
-        <v-flex xs='12' sm='6' md='4'>コンテンツ</v-flex>
-    <v-flex xs='12' sm='6' md='4'>コンテンツ</v-flex>!-->
-  </v-layout>
+  <v-hover v-slot:default="{ hover }" open-delay="200">
+    <v-card
+      v-if="
+        $store.state.filter_grade === grade ||
+          (!$store.state.filter_grade && !$store.state.filter_major)
+      "
+      :elevation="hover ? 16 : 2"
+      class="d-flex mx-auto card"
+      max-width="400"
+    >
+      <v-responsive
+        class="text-center grey lighten-2 rounded-circle d-inline-flex align-center justify-center ma-3"
+        height="108"
+        width="108"
+      >
+        <v-img v-bind:src="img"></v-img>
+      </v-responsive>
+      <v-card-text>
+        <p class="display-1 text--primary">{{ name }}</p>
+        <p>{{ grade }}年 {{ major }} {{ group }}</p>
+        <div class="text--primary">
+          最近はサーバー構築やってるよ!
+          <br />気軽に話しかけてね
+        </div>
+        <v-card-actions></v-card-actions>
+      </v-card-text>
+    </v-card>
+  </v-hover>
 </template>
 <script>
-import Grade from "./grade";
-
 export default {
+  props: ["img", "name", "grade", "major", "group"],
   data: () => ({}),
-  methods: {
-    reset_filter(filter) {
-      if (filter === "選択") {
-        this.filter_grade = "";
-      }
-    },
-  },
-  components: {
-    Grade,
-  },
+  methods: {},
 };
 </script>
 <style>
--- docker/vue/src/pages/Home.vue
+++ docker/vue/src/pages/Home.vue
@@ -3,16 +3,16 @@
     <!-- fluidを付けることで左右いっぱいにコンテナが広がります -->
     <!-- fill-heightを付けるとここに書いた要素は上下中央寄せで表示されます -->
     <Filters />
-    <UserCard />
+    <UserCards />
   </v-container>
 </template>
 
 <script>
-import UserCard from "../components/user_card";
+import UserCards from "../components/user_cards";
 import Filters from "../components/filter";
 export default {
   components: {
-    UserCard,
+    UserCards,
     Filters,
   },
   data: () => ({}),
