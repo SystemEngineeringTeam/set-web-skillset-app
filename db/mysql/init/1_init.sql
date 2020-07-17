ef865aebf720b237237d1546c7368a46e65e7a55
kenshin1025 <sukoyakamind1025@gmail.com>
2020-07-17 22:08:54 +0900

    データベース設計ができた

--- db/mysql/init/1_init.sql
+++ db/mysql/init/1_init.sql
@@ -1,50 +1,40 @@
 CREATE DATABASE IF NOT EXISTS skillset;
 USE skillset;
-CREATE TABLE skillset.users (
-    user_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    user_email VARCHAR (255) NOT NULL,
-    user_name VARCHAR (255) NOT NULL,
-    user_password VARCHAR (255) NOT NULL
-);
-INSERT INTO users (user_email, user_name, user_password)
-VALUES ("a@gmail.com", "たろう", "pass");
-CREATE TABLE skillset.grades (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    text VARCHAR (255) NOT NULL
-);
-INSERT INTO grades (text)
-VALUES ("1年");
-CREATE TABLE skillset.majors (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    text VARCHAR (255) NOT NULL
-);
-CREATE TABLE skillset.groups (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    text VARCHAR (255) NOT NULL
-);
-CREATE TABLE skillset.technical_areas (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    text VARCHAR (255) NOT NULL
-);
-CREATE TABLE skillset.technologies (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    text VARCHAR (255) NOT NULL
-);
 CREATE TABLE skillset.members (
     id VARCHAR NOT NULL PRIMARY KEY,
     image VARCHAR,
-    name VARCHAR,
+    name VARCHAR NOT NULL,
     message VARCHAR,
-    slack VARCHAR,
+    slack VARCHAR NOT NULL,
     twitter VARCHAR,
     github VARCHAR,
-    grade_id INT,
-    major_id INT,
+    grade_id INT NOT NULL,
+    major_id INT NOT NULL,
     created_at DATETIME default current_timestamp,
     updated_at DATETIME default current_timestamp ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT fk_grade_id FOREIGN KEY (grade_id) REFERENCES grades (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-    CONSTRAINT fk_major_id FOREIGN KEY (major_id) REFERENCES majors (id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
+INSERT INTO members (
+        id,
+        image,
+        name,
+        message,
+        slack,
+        twitter,
+        github,
+        grade_id,
+        major_id,
+    )
+VALUES (
+        "550e8400-e29b-41d4-a716-446655440000",
+        "https://3.bp.blogspot.com/-A_AqujoUstw/Uj_279AEvZI/AAAAAAAAYEk/C7AU-QH0IiE/s800/monogatari_sanbikinokobuta.png"
+        "三匹の子豚",
+        "message",
+        "ぶたさん",
+        "butasan",
+        "butasan",
+        1,
+        1,
+    );
 CREATE TABLE skillset.groups_of_members (
     member_id VARCHAR NOT NULL PRIMARY KEY,
     group_id INT NOT NULL PRIMARY KEY,
@@ -59,9 +49,9 @@ CREATE TABLE skillset.technical_areas_of_members (
 );
 CREATE TABLE skillset.technologies_of_members (
     member_id VARCHAR NOT NULL PRIMARY KEY,
-    technologie_id INT NOT NULL PRIMARY KEY,
+    technology_id INT NOT NULL PRIMARY KEY,
     CONSTRAINT fk_member_id FOREIGN KEY (member_id) REFERENCES members (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-    CONSTRAINT fk_technologie_id FOREIGN KEY (technologie_id) REFERENCES technologies (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+    CONSTRAINT fk_technology_id FOREIGN KEY (technology_id) REFERENCES technologies (id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 CREATE TABLE skillset.products_of_members (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
