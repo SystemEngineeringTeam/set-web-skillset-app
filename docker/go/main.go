1520997d142638d2fc2b3905cebafaea9a97f792
kenshin1025 <sukoyakamind1025@gmail.com>
2020-07-23 22:45:58 +0900

    getでjsonの配列が返せるようになった、あとはjson内の配列の実装をこれからする

--- docker/go/main.go
+++ docker/go/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// Member 名簿に登録されている会員情報一人分を表す型
 type Member struct {
 	ID        string    `db:"id"`
 	Image     string    `db:"image"`
@@ -24,38 +26,79 @@ type Member struct {
 	UpdatedAt time.Time `db:"updated_at"`
 }
 
+type product struct {
+	Overview string `json:"overview"`
+	URL      string `json:"url"`
+}
+
+type ResponseMember struct {
+	ID            string    `json:"id"`
+	Image         string    `json:"image"`
+	Name          string    `json:"name"`
+	Message       string    `json:"message"`
+	Slack         string    `json:"slack"`
+	Twitter       string    `json:"twitter"`
+	Github        string    `json:"github"`
+	GradeID       int       `json:"grade_id"`
+	MajorID       int       `json:"major_id"`
+	Group         []int     `json:"group"`
+	TechnicalArea []int     `json:"technical_area"`
+	Technology    []int     `json:"technology"`
+	Product       []product `json:"product"`
+}
+
 func get(w http.ResponseWriter, r *http.Request, db *sql.DB) {
-	rows, err := db.Query("SELECT name FROM members")
+	rows, err := db.Query("SELECT * FROM members")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
 
+	var response []ResponseMember
+
 	for rows.Next() {
 		var member Member
-		if err := rows.Scan(&member.Name); err != nil {
+		if err := rows.Scan(&member.ID, &member.Image, &member.Name, &member.Message, &member.Slack, &member.Twitter, &member.Github, &member.GradeID, &member.MajorID, &member.CreatedAt, &member.UpdatedAt); err != nil {
 			log.Fatal(err)
 		}
-		fmt.Fprintf(w, member.Name)
-		fmt.Fprintf(w, "こんにちは")
-		fmt.Println(member.Name)
-		fmt.Println("こんにちは")
+
+		var responseMember ResponseMember
+		responseMember.ID = member.ID
+		responseMember.Image = member.Image
+		responseMember.Name = member.Name
+		responseMember.Message = member.Message
+		responseMember.Slack = member.Slack
+		responseMember.Twitter = member.Twitter
+		responseMember.Github = member.Github
+		responseMember.GradeID = member.GradeID
+		responseMember.MajorID = member.MajorID
+
+		response = append(response, responseMember)
 	}
 	if err := rows.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	responseJSON, err := json.Marshal(&response)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, string(responseJSON))
 }
 
 func main() {
 	fmt.Printf("Starting server at 'http://localhost:8082/'\n")
+	fmt.Printf("goto'http://localhost:8082/member/get'\n")
 
-	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/skillset")
+	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/skillset?parseTime=true&loc=Asia%2FTokyo")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/member/get", func(w http.ResponseWriter, r *http.Request) {
 		get(w, r, db)
 	})
 	http.ListenAndServe(":8082", nil)
